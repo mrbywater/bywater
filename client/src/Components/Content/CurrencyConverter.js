669e15d00ed9e0ee75a440d1093cd8d7521d1f36
mrbywater <acaramelb228@gmail.com>
2023-09-28 18:59:48 +0300

    add date picker

--- client/src/Components/Content/CurrencyConverter.js
+++ client/src/Components/Content/CurrencyConverter.js
@@ -1,13 +1,41 @@
 import './Main.scss'
 import './CurrencyConverter.scss'
-import { useState } from 'react'
+import axios from "axios"
+import { useState, useRef, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
-import { DatePicker } from './DatePicker.js';
+import DatePicker from "react-multi-date-picker";
+import InputIcon from "react-multi-date-picker/components/input_icon"
+import "react-multi-date-picker/styles/layouts/mobile.css"
+import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
+// import { DatePicker } from './DatePicker.js';
 
 const CurrencyConverter = () => {
 
-	const [dateValue, setDateValue] = useState('')
+	const datePickerRef = useRef();
+
+	const apiKey = '5e24c6fb385decb5925799b3870f8d74'
+
+	const [dateValue, setDateValue] = useState(new Date())
+	const [currrency, setCurrency] = useState(null)
+
+	useEffect(()=> {
+		currrencyAPI()
+	}, [])
+	
+	const currrencyAPI = async () => {
+		try {
+		    const res = await axios.get(`https://api.exchangeratesapi.io/v1/latest?access_key=${apiKey}`)
+
+		    return (
+		    	setCurrency([res])
+		    )
+		  } catch (err) {
+		    console.error(err.toJSON())
+		  }
+	}
+
+	
 
 	return (
 		<div className="mainContainerContent">
@@ -19,7 +47,10 @@ const CurrencyConverter = () => {
 							<div className="currencyValue"></div>
 							<div className="fastCurrency"></div>
 						</div>
-						<div className="currencySwap">
+						<div 
+							className="currencySwap"
+							onClick={()=> console.log(currrency)}
+						>
 							<FontAwesomeIcon icon={faRightLeft} />
 						</div>
 						<div>
@@ -33,7 +64,25 @@ const CurrencyConverter = () => {
 							<span>
 								Date
 							</span>
-							<DatePicker/>  
+							<DatePicker 
+								ref={datePickerRef}
+								className="rmdp-mobile bg-dark"
+							    value={dateValue}
+							    onChange={setDateValue}
+							    maxDate={new Date().setDate(15)}
+							    mobileButtons={[
+							      {
+							        label: "TODAY",
+							        type: "button",
+							        className: "rmdp-button rmdp-action-button",
+							        onClick: () => {
+							        	setDateValue(new Date())
+							        	datePickerRef.current.closeCalendar()
+							        },
+							      },
+							    ]}
+								render={<InputIcon/>}
+							/>
 						</div>
 					</div>
 				</div>
