5e9759aabcf01acdf2c9fdd4a4397eb02a28ea4d
mrbywater <acaramelb228@gmail.com>
2023-10-08 00:24:23 +0300

    test4

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -81,14 +81,14 @@ const Header = () => {
 				    onClose={handleIsOpen}
 			    >
 					<div className="headerLogo">
-						<Link to={'bywater/'}> 
+						<Link to={'/'}> 
 							<img src={Logo} alt="logo" onClick={closeSideBar}/>
 						</Link>		
 					</div>
 			       <div className="buttonsHeaderContainer">
 						{headerButtons.map(i =>
 							<Link 
-								to={i.name === "Home" ? "bywater/" : `bywater/${linkConvert(i.name)}`} 
+								to={i.name === "Home" ? "/" : linkConvert(i.name)} 
 								key={`name_${i.name}`} 
 								onClick={closeSideBar}
 							> 
@@ -117,7 +117,7 @@ const Header = () => {
 			</div>
 			<div className="buttonsHeaderContainer">
 				{headerButtons.map(i =>
-					<Link to={i.name === "Home" ? "bywater/" : `bywater/${linkConvert(i.name)}`}> 
+					<Link to={i.name === "Home" ? "/" : linkConvert(i.name)} key={`name_${i.name}`}> 
 						<div className="buttonsHeaderLabel">
 							<FontAwesomeIcon icon={i.label} style={((i.name === "Home" ? '' : linkConvert(i.name)) === location) ? {color: i.selectColor} : ''}/>
 						</div>
